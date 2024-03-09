de6e6ec0100b9d917ddf05031d392b5eea9446c8
James Kong <104340270+jameskongchief@users.noreply.github.com>
2024-03-09 13:12:38 +0800

    fixed python script

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ def convert_row_to_html(row, row_number):
     
     html_template = f"""
     <!DOCTYPE html>
-    <html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Submission Detail - {name}</title>
@@ -38,7 +37,9 @@ def convert_row_to_html(row, row_number):
                     </div>
                 """
             else:
-                html_template += f"<p>{cell}</p>"
+                html_template += f"""
+                <p style="font-family: Arial, sans-serif;">{cell}</p>
+                """
     
     html_template += """
         </div>
